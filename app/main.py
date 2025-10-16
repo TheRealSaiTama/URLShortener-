3140606398b2eb4e6cf3c2b15557aed1151c00fa
Keshav Kumar Jha <110764833+TheRealSaiTama@users.noreply.github.com>
2025-10-17 00:08:16 +0530

    feat: 307 redirects

--- app/main.py
+++ app/main.py
@@ -4,6 +4,8 @@ from sqlalchemy import select
 from .db import Base, engine, SessionLocal
 from .models import UrlMap
 import hashlib
+from starlette.responses import RedirectResponse
+
 
 app = FastAPI(title="url-shorty", version="0.2")
 Base.metadata.create_all(engine)
@@ -32,3 +34,10 @@ def resolve(code: str):
         row = s.execute(select(UrlMap).where(UrlMap.code == code)).scalar_one_or_none()
         if not row: raise HTTPException(404, "code not found")
         return {"target": row.target}
+
+@app.get("/{code}")
+def resolve(code: str):
+    with SessionLocal() as s:
+        row = s.execute(select(UrlMap).where(UrlMap.code == code)).scalar_one_or_none()
+        if not row: raise HTTPException(404, "code not found")
+        return RedirectResponse(row.target)
